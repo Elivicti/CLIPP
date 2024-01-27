8b136afb1f7ce330fca32f899a4dfedb65fccb30
Elivicti <edwynb72@gmail.com>
2024-01-27 19:36:33 +0800

    update example

--- test/main.cpp
+++ test/main.cpp
@@ -1,20 +1,23 @@
 #include "../include/CLI++/CLI++.hpp"
-#include "../include/CLI++/StringHelper.hpp"
+#include <filesystem>
 
 SET_CLIPP_ALIAS(CLI);
 
-int main()
+int main(int argc, const char** argv)
 {
-	std::string name{"USER"};
-	std::string dir{"~/CLI++"};
-
-	std::string colored_prompt = fmt::format(
+	CLI::String name{"USER"};
+	CLI::String dir{"~/CLI++"};
+	CLI::String colored_prompt = fmt::format(
 		"{}:{}$ "
 		, CLI::styled(name, fmt::fg(fmt::rgb(0x16c60c)))
 		, CLI::styled(dir,  fmt::fg(fmt::rgb(0x3b78ff)))
 	);
 	CLI::CLI app{colored_prompt};
-	int ret = app.mainloop();
+	app.insertCommand("pwd", [](CLI::CLI*, const CLI::ArgList& args) {
+		fmt::print("pwd: {}\n", std::filesystem::current_path().string());
+	}, "print working directory");
+
+	int ret = app.exec();
 	fmt::print("CLI returned with code: {}\n", ret);
 	return ret;
 }
