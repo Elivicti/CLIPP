6a1378f88950e5d93947c488aad5cae319add103
Elivicti <edwynb72@gmail.com>
2024-01-27 19:36:12 +0800

    added more documentation, implemented usage() and better completion for CLICommand

--- include/CLI++/CLI++.hpp
+++ include/CLI++/CLI++.hpp
@@ -3,63 +3,224 @@
 
 #include "defines.hpp"
 #include "Exceptions.hpp"
-#include "ArgParser.hpp"
+#include "detail.hpp"
 
 #include <map>
 #include <functional>
+#include <fmt/color.h>
 
 __CLIPP_begin
 
+using ArgList = std::vector<String>;
+class CLI;
+
 class CLICommand
 {
-public:
-	CLICommand(const std::string& cmd, const std::string& doc = std::string())
-		: cmd(cmd), doc(doc) {}
+protected:
+	struct OptionType
+	{
+		String name;
+		char short_name;
+		String desc;
 
-	const std::string& name() const { return cmd; }
-	const std::string& desc() const { return doc; }
-	std::string& name() { return cmd; }
-	std::string& desc() { return doc; }
+		// if either of the name or short name is same, options should be considered the same
+		bool operator==(const OptionType& opt) const
+		{
+			return name == opt.name
+				|| ((short_name == opt.short_name) && (short_name != 0));
+		}
+		bool operator<(const OptionType& opt) const
+		{ return name < opt.name; }
+	};
+public:
+	using CommandHandler = std::function<void(CLI*, const ArgList&)>;
+public:
+	CLICommand(const String& cmd, CommandHandler func, const String& desc = String())
+		: cmd(cmd), desc(desc), handler(func), options() {}
+	virtual ~CLICommand() = default;
+
+	const String& name() const { return cmd; }
+	const String& description() const { return desc; }
+	String& name() { return cmd; }
+	String& description() { return desc; }
+
+	/**
+	 * @brief Try to match the text with this command (which is usually partially matched),
+	 *        this function also tries to match its options or sub commands.
+	 * @param text text to be matched
+	 * @param len length of the text
+	 * @return C-Style string pointer, if not matched, nullptr is returned.
+	 * @note If you want to reimplement this virtual function, you should make sure that
+	 *       returned string pointer is created through "malloc" or "strdup".
+	**/
+	virtual char* match(const char* text, int len) const;
+	virtual String usage() const;
+
+	/**
+	 * @brief Add an option to this command.
+	 * @note  Options are only used in completion and to generate usage string, CLI won't check if input meet
+	 *        command's requirement.
+	 * @param opt option name
+	 * @param short_name short option name
+	 * @param desc description for this option
+	**/
+	void addOption(const String& opt, char short_name = '\0', const String& desc = String());
+	/**
+	 * @brief Remove an option from this command.
+	 * @param opt option name
+	**/
+	void removeOption(const String& opt);
+	/**
+	 * @brief Remove an option from this command.
+	 * @param short_name short option name, if equals to '\0', this function do nothing
+	**/
+	void removeOption(char short_name);
+	/**
+	 * @brief Add a sub command to this command.
+	 * @note  Sub commands are only used in completion and to generate usage string, CLI won't check if input meet
+	 *        command's requirement.
+	 * @param opt sub command name
+	 * @param desc description for this sub command
+	**/
+	void addSubCommand(const String& subcmd, const String& desc = String());
+	/**
+	 * @brief Remove a sub command from this command.
+	 * @param subcmd sub command name
+	**/
+	void removeSubCommand(const String& subcmd);
+
+protected:
+	int cursorPos() const { return pos; }
 
 private:
-	std::string cmd;
-	std::string doc;
+	String cmd;
+	String desc;
 
+	CommandHandler handler;
+
+	std::vector<OptionType> options;
+	std::vector<OptionType> subcmds;
+
+	static int pos;	//used to determine whether to complete command or its arguments
+
+	friend char** command_completion(const char* text, int start, int end);
 public:
 	bool operator==(const CLICommand& other) const { return cmd == other.cmd; }
 	bool operator< (const CLICommand& other) const { return cmd <  other.cmd; }
-};
 
+	virtual void operator()(CLI* cli, const ArgList& args) const { handler(cli, args); }
+};
 
 class CLI
 {
 public:
-	using CommandHandler = std::function<void(const std::vector<std::string>&)>;
-public:
-	CLI(const std::string& prompt, char completion_key = '\t');
+	CLI(const String& prompt = String("CLI> "), char completion_key = '\t');
+	~CLI();
 
-	int mainloop();
+	int exec();
 
-	void setPrompt(const std::string& prompt) { this->prompt = prompt; };
+	void setPrompt(const String& prompt) { this->prompt = prompt; };
 
-	void insertCommand(const std::string& cmd, CommandHandler f, const std::string& doc = std::string())
+	/**
+	 * @brief Create a command with specific name, description and action.
+	 * @note  If a command with same name already exists, the old one will be deleted.
+	 * @param name command name
+	 * @param f function to be called
+	 * @param desc description for this command
+	**/
+	void insertCommand(const String& name, CLICommand::CommandHandler f, const String& desc = String())
 	{
-		commands.insert_or_assign(CLICommand{cmd, doc}, f);
+		if (commands.contains(name))
+			delete commands.at(name);
+		auto* command = new CLICommand(name, f, desc);
+		this->updateHelp(command);
+		commands.insert_or_assign(name, command);
+	}
+	/**
+	 * @brief Insert a CLICommand or its derived class intance.
+	 * @note  This function will take pointer's ownership, and delete it in deconstructor.
+	 *        If a command with same name already exists, the old one will be deleted.
+	 * @param command pointer to a CLICommand or its derived class intance, must be created with new operator
+	**/
+	void insertCommand(CLICommand* command)
+	{
+		if (commands.contains(command->name()))
+			delete commands.at(command->name());
+		this->updateHelp(command);
+		commands.insert_or_assign(command->name(), command);
 	}
 
+	/**
+	 * @brief Checks if CLI contains a command with specific name.
+	 * @param name command name to match
+	**/
+	bool contains(const String& name) const { return commands.contains(name); }
+
+	/**
+	 * @brief Remove a CLICommand or its derived class intance pointer from CLI, and its ownership.
+	 * @note  This function will not delete the instance.
+	 * @param name command name to match
+	 * @return return nullptr if command with specified name does not exists
+	**/
+	CLICommand* take(const String& name)
+	{
+		if (!commands.contains(name))
+			return nullptr;
+		CLICommand* ret = commands.at(name);
+		commands.erase(name);
+		return ret;
+	}
+	/**
+	 * @brief Find command by name
+	 * @param name command name to be matched
+	 * @return Pointer to the command found, if no matching command, nullptr is returned.
+	**/
+	CLICommand* command(const String& name)
+	{
+		if (!commands.contains(name))
+			return nullptr;
+		return commands.at(name);
+	}
+	const CLICommand* command(const String& name) const
+	{
+		if (!commands.contains(name))
+			return nullptr;
+		return commands.at(name);
+	}
+
+public:
+	/** @brief Exit CLI exec loop with given code. Do nothing if called outside exec loop. */
+	void exit(int code = 0) const;
+
+	/** @brief Just an echo. */
+	void echo(const ArgList& args) const;
+	/** @brief List all available commands or print help for specified command. */
+	void help(const ArgList& args) const;
+	/** @brief Clear screen. */
+	void clearScreen() const;
 private:
-	using CmdCallBackMap = std::map<CLICommand, CommandHandler>;
+	void exitImpl(const ArgList& args) const;
+	void updateHelp(const CLICommand* cmd);
+private:
+	using CLICommandMap = std::map<String, CLICommand*>;
 
 	static CLI* cli_instance;
 	static CLI* instance() { return cli_instance; }
 
-	friend char*  command_generator(const char *text, int state);
-	friend char** command_completion(const char *text, int start, int end);
+	friend char*  command_generator(const char* text, int state);
+	friend char** command_completion(const char* text, int start, int end);
 private:
-
-	std::string prompt;
-	CmdCallBackMap commands;
+	bool in_exec_loop;
+	String prompt;
+	CLICommandMap commands;
 };
+
+template <typename T>
+FMT_CONSTEXPR auto styled(const T& value, fmt::text_style ts)
+	-> detail::styled_arg<fmt::remove_cvref_t<T>>
+{
+	return detail::styled_arg<fmt::remove_cvref_t<T>>{value, ts};
+}
 
 __CLIPP_end
 #endif //! __CLIPP_INTERACTIVE_HEADER__
--- src/CLI++.cpp
+++ src/CLI++.cpp
@@ -1,19 +1,21 @@
 #include "../include/CLI++/CLI++.hpp"
-#include "../include/CLI++/StringHelper.hpp"
+#include "../include/CLI++/detail.hpp"
+#include <sstream>
+#include <ranges>
 
 #include <readline/readline.h>
 #include <readline/history.h>
 
 __CLIPP_begin
 
+///////////////// Readline API /////////////////
 char* command_generator(const char* text, int state)
 {
 	static int len = 0;
-	static CLI::CmdCallBackMap::iterator begin, end;
-	
-	/* If this is a new word to complete, initialize now. This includes
-	 * saving the length of TEXT for efficiency, and initializing the index
-	 * variable to 0. */
+	static CLI::CLICommandMap::iterator begin, end;
+
+	// if this is a new word to complete, initialize now.
+	// this includes saving the length of TEXT for efficiency, and initializing the iterator.
 	if (state == 0)
 	{
 		begin = CLI::cli_instance->commands.begin();
@@ -21,30 +23,30 @@ char* command_generator(const char* text, int state)
 		len = strlen(text);
 	}
 
-	/* Return the next name which partially matches from the command list. */
+	// return the next name which partially matches from the command list.
 	while (begin != end)
 	{
-		auto& [ cmd, func ] = *begin;
+		auto& [ name, cmd ] = *begin;
 		++begin;
 
-		if (strncmp(cmd.name().data(), text, len) == 0)
-			return strdup(cmd.name().data());
+		if (char* matched = cmd->match(text, len))
+			return matched;
 	}
-	return ((char *)NULL);
+	return rl_filename_completion_function(text, state);
 }
 char** command_completion(const char* text, int start, int end)
 {
 	char** matches = (char**)NULL;
-	/* If this word is at the start of the line, then it is a command
-	 * to complete. Otherwise it is the name of a file in the current directory. */
-	if (start == 0)
-		matches = rl_completion_matches(text, command_generator);
+	// store start position so that CLICommand can find out whether to complete command or arguments
+	CLICommand::pos = start;
+	matches = rl_completion_matches(text, command_generator);
 	return (matches);
 }
 
+
+/////////////// Special Excepion ///////////////
 class CLIExcepionExit : public CLIExcpetion
 {
-
 public:
 	CLIExcepionExit(int _code = 0) noexcept
 		: CLIExcpetion("This CLIExcpetion is called to exit CLI with return code."), _code(_code) {}
@@ -55,79 +57,260 @@ private:
 	int _code;
 };
 
+
+////////////////// CLICommand //////////////////
+int CLICommand::pos = 0;
+char* CLICommand::match(const char* text, int len) const
+{
+	if (pos == 0 && cmd.compare(0, len, text) == 0)
+		return detail::strdup(cmd.data());
+
+	if (pos != 0 && len > 0)
+	{
+		// search sub commands first
+		auto subcmd_it = std::ranges::find_if(
+			subcmds,
+			[text, len](const OptionType& opt) {
+				return opt.name.compare(0, len, text) == 0;
+			}
+		);
+		if (subcmd_it != subcmds.cend())
+			return detail::strdup(subcmd_it->name.data());
+	}
+	if (pos != 0 && len >= 2 && std::strncmp(text, "--", 2) == 0)
+	{
+		// search for options
+		auto opt_it = std::ranges::find_if(
+			options,
+			[text, len](const OptionType& opt) {
+				return opt.name.compare(0, len - 2, text + 2) == 0;
+			}
+		);
+		if (opt_it != subcmds.cend())
+			return detail::strdup(String("--" + opt_it->name).data());
+	}
+
+	return nullptr;
+}
+String CLICommand::usage() const
+{
+	// bool has_subcmd = subcmds.size() > 0;
+	// bool has_option = options.size() > 0;
+
+	// if (!has_subcmd && !has_option)
+	// 	return "";
+
+	std::ostringstream ss;
+	if (subcmds.size() > 0)
+	{
+		ss << "sub commands:\n";
+		std::size_t max_len = std::ranges::max(
+			std::views::transform(subcmds,
+			[](const OptionType& opt) { return opt.name.size(); }
+		));
+		for (auto& sub : subcmds)
+			ss << fmt::format("  {:<{}} {}\n", sub.name, max_len + 1, sub.desc);
+	}
+
+	if (options.size() > 0)
+	{
+		std::size_t max_len = std::ranges::max(
+			std::views::transform(options,
+			[](const OptionType& opt) { return opt.name.size(); }
+		));
+
+		ss << "options:\n";
+		for (auto& opt : options)
+			ss << fmt::format("  {:3s} --{:<{}} {}\n"
+					, opt.short_name ? fmt::format("-{},", opt.short_name) : ""
+					, opt.name, max_len + 1
+					, opt.desc);
+	}
+
+	return ss.str();
+}
+
+void CLICommand::addOption(const String& opt_name, char short_name, const String& desc)
+{
+	OptionType opt{ opt_name, short_name, desc };
+	auto it = std::ranges::find(options, opt);
+	if (it == options.end())
+		options.insert(it, opt);
+	else
+		*it = opt;
+}
+void CLICommand::addSubCommand(const String& subcmd_name, const String& desc)
+{
+	OptionType cmd{ subcmd_name, 0, desc };
+	auto it = std::ranges::find(subcmds, cmd);
+	if (it == subcmds.end())
+		subcmds.insert(it, cmd);
+	else
+		*it = cmd;
+}
+void CLICommand::removeOption(const String& opt_name)
+{
+	auto it = std::ranges::find_if(
+		options, [&opt_name](const OptionType& opt) {
+			return opt.name == opt_name;
+		}
+	);
+	if (it != subcmds.end())
+		options.erase(it);
+}
+void CLICommand::removeOption(char short_name)
+{
+	if (short_name == 0)
+		return;
+	auto it = std::ranges::find_if(
+		options, [short_name](const OptionType& opt) {
+			return opt.short_name == short_name;
+		}
+	);
+	if (it != subcmds.end())
+		options.erase(it);
+}
+void CLICommand::removeSubCommand(const String& subcmd)
+{
+	auto it = std::ranges::find_if(
+		subcmds, [&subcmd](const OptionType& opt) {
+			return opt.name == subcmd;
+		}
+	);
+	if (it != subcmds.end())
+		subcmds.erase(it);
+}
+
+//////////////////    CLI     //////////////////
 CLI* CLI::cli_instance = nullptr;
 
-CLI::CLI(const std::string& prompt, char completion_key)
-	: prompt(prompt)
+CLI::CLI(const String& prompt, char completion_key)
+	: in_exec_loop(false), prompt(prompt)
 {
 	if (cli_instance != nullptr)
 		throw CLIExcpetion("CLI can ONLY have one instance.");
 	cli_instance = this;
 
 	rl_bind_key(completion_key, rl_complete);
-	
 	rl_attempted_completion_function = command_completion;
 
-	commands.insert(std::make_pair("cd", [](const std::vector<std::string>&) { fmt::print("chdir!\n"); }));
-	commands.insert(std::make_pair("help", [](const std::vector<std::string>&) { fmt::print("help!\n"); }));
-	commands.insert(
-		std::make_pair("echo",
-		[](const std::vector<std::string>& args) {
-			size_t count = args.size();
-			for (size_t i = 1; i < count; i++)
-				fmt::print("argv[{}] = {:?}\n", i, args[i]);
-		})
-	);
-	commands.insert(
-		std::make_pair("clear",
-		[](const std::vector<std::string>& args) {
-		})
-	);
-	commands.insert(
-		std::make_pair("exit", [](const std::vector<std::string>& args) { throw CLIExcepionExit(0); })
-	);
-	
+	commands.emplace("help", new CLICommand("help", [](CLI* cli, const ArgList& args) {
+		cli->help(args);
+	}, "list all available commands or print help for specified command"));
+	commands.emplace("echo", new CLICommand("echo", [](CLI* cli, const ArgList& args) {
+		cli->echo(args);
+	}, "just an echo"));
+	commands.emplace("clear", new CLICommand("clear", [](CLI* cli, const ArgList&) {
+		cli->clearScreen();
+	}, "clear screen"));
+	commands.emplace("exit", new CLICommand("exit", [](CLI* cli, const ArgList& args) {
+		cli->exitImpl(args);
+	}, "exit cli with return code, if not specified, return 0"));
+
+	auto cmd_help = commands.at("help");
+	for (auto& [ cmd_name, cmd ] : commands)
+		cmd_help->addSubCommand(cmd_name, cmd->description());
+	// fmt::print("{}: {}\n{}\n", cmd_help->name(), cmd_help->description(), cmd_help->usage());
 }
 
-int CLI::mainloop()
+CLI::~CLI()
 {
-	int ret_code = 0;
+	for (auto& [name, cmd] : commands)
+	{
+		delete cmd;
+	}
+}
+
+void CLI::exit(int code) const
+{
+	if (in_exec_loop)
+		throw CLIExcepionExit(code);
+}
+void CLI::exitImpl(const ArgList& args) const
+{
+	int code = 0;
+	if (args.size() > 1)
+	{
+		try { code = std::stoi(args[1]); }
+		catch(const std::exception&)
+		{
+			throw CLIExcpetion("Invalid exit code.");
+		}
+	}
+	this->exit(code);
+}
+void CLI::echo(const ArgList& args) const
+{
+	// fmt::print("{}\n", fmt::join(args.begin() + 1, args.end(), " "));
+	size_t count = args.size();
+	for (size_t i = 0; i < count; i++)
+		fmt::print("argv[{}] = {}\n", i, args[i]);
+}
+void CLI::help(const ArgList& args) const
+{
+	const auto& cmds = this->commands;
+	if (args.size() < 2)
+	{
+		fmt::print("available commands:\n");
+		for (const auto& [ key, _ ] : cmds)
+			fmt::print("{}  ", key);
+		fmt::print("\n");
+		return;
+	}
+	auto& cmd = args[1];
+	if (!cmds.contains(cmd))
+		throw CLIExcpetion("help: No such command");
+	else
+	{
+		auto pcmd = cmds.at(cmd);
+		fmt::print("{}: {}\n{}", cmd, pcmd->description(), pcmd->usage());
+	}
+}
+void CLI::clearScreen() const
+{
+	fmt::print("\x1b\x5b\x48\x1b\x5b\x32\x4a");
+}
+void CLI::updateHelp(const CLICommand* cmd)
+{
+	if (!commands.contains("help"))
+		return;
+	auto* cmd_help = commands.at("help");
+	cmd_help->addSubCommand(cmd->name(), cmd->description());
+}
+
+int CLI::exec()
+{
+	in_exec_loop = true;
 	while (true)
 	{
-		std::unique_ptr<char> raw_input;
-		// use unique_ptr to handle the pointer in case an exception shows up while handling commands
-		raw_input.reset(readline(prompt.data()));
+		char* raw_input = readline(prompt.data());
 		if (!raw_input)
 			break;
+		String input(raw_input);
+		free(raw_input);
 
-		std::string input(raw_input.get());
+		if (detail::is_empty_string(input))
+			continue;
+
 		add_history(input.data());
-
-		// TODO:
-		// [DONE] remove leading and trailing spaces in the input
-		// [DONE] parse the input into some format
-		//        -> std::vector<std::string>
 
 		try
 		{
-			const auto& argv = str_to_argv(input);
+			const auto& argv = detail::string_to_argv(input);
 
 			if (commands.contains(argv[0]))
-				commands.at(argv[0])(argv);
+				commands.at(argv[0])->operator()(this, argv);
 			else if (!input.empty())
 				fmt::print("{}: No such command\n", argv[0]);
 		}
+		catch(const CLIExcepionExit& exit) { return exit.code(); }
 		catch(const std::exception& e)
 		{
-			if (auto* exit = dynamic_cast<const CLIExcepionExit*>(&e))
-			{
-				ret_code = exit->code();
-				break;
-			}
 			fmt::print("{}\n", e.what());
 		}
 	}
-	return ret_code;
+	in_exec_loop = false;
+	return 0;
 }
 
 __CLIPP_end
