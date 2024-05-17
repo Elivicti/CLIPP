b8596e20fcc5fbe29a9af0ad657a9a851e125129
Elivicti <edwynb72@gmail.com>
2024-05-17 10:15:46 +0800

    renamed ArgParser.cpp

---
+++ src/ArgumentParser.cpp
@@ -0,0 +1,20 @@
+#include "../include/CLI++/ArgumentParser.hpp"
+#include "../include/CLI++/detail.hpp"
+
+CLIPP_BEGIN
+
+REGISTER_TYPENAME(String, "string");
+
+void ArgParser::parse(int argc, const char** argv)
+{
+
+}
+
+void ArgParser::parse(const std::vector<String>& args)
+{
+
+}
+
+
+
+CLIPP_END
